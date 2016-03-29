bbeb68293115bcb6c66c00bf2f799d6034353266
Julian Harley <julian.harley@pancentric.com>
2016-03-29 21:18:04 +0100

    give nodes an id

--- map/mapping_elements.py
+++ map/mapping_elements.py
@@ -2,5 +2,6 @@
 
 class Node(object):
 
-    def __init__(self,name):
+    def __init__(self,name='anon',id=-1):
         self.name = name
+        self.id = id
--- map/tests.py
+++ map/tests.py
@@ -3,9 +3,14 @@ from mapping_elements import Node
 
 class TestMap(unittest.TestCase):
 
-    def test_node(self):
+    def test_simple_create_node(self):
         mapnode = Node(name='Aldgate')
         self.assertEquals(mapnode.name, 'Aldgate')
+
+    def test_simple_create_node_with_id(self):
+        mapnode = Node(name='Aldgate',id = 2)
+        self.assertEquals(mapnode.name, 'Aldgate')
+        self.assertEquals(mapnode.id, 2)
 
 if __name__ == '__main__':
     unittest.main()
